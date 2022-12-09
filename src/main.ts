7b6fd5f5bc65c1c8a22a8bda9a3a691060ac8ef5
player31-kks <keumks789@naver.com>
2022-12-09 22:48:19 +0900

    [Add] basic http data

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,19 @@
-console.log("hello")
+import { createServer } from 'net';
+
+const server = createServer((socket) => {
+  socket.on('data', (data) => {
+    console.log(data.toString());
+    const content = 'hello world';
+    socket.write(Buffer.from(`HTTP/1.1 200 OK\r\n`));
+    socket.write(Buffer.from(`Content-Type: text/plain\r\n`));
+    socket.write(Buffer.from(`Content-Length: ${content.length}\r\n`));
+    socket.write(Buffer.from(`\r\n`));
+    socket.write(content);
+    socket.write(Buffer.from(`\r\n`));
+    socket.end();
+  });
+});
+
+server.listen(8080, () => {
+  console.log('server open');
+});
