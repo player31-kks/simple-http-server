5ecc3f1c973b97eb4e57134af731b58abde2f480
player31-kks <keumks789@naver.com>
2023-01-15 13:45:08 +0900

    [fix]: change buildspec node version

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,7 +4,7 @@
 sudo apt-get update
 
 # Install Node.js and npm
-NODE_VERSION=18.12.0
+NODE_VERSION=16.16.0
 curl -sL https://deb.nodesource.com/setup_$NODE_VERSION | sudo -E bash -
 sudo apt-get install -y nodejs
 
